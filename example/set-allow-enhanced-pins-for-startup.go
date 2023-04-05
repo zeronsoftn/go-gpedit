ee9f236fd2a7649caa46b5bd07b5adb4852d3bcf
hsh2023 <hsh2023@zeronsoftn.com>
2023-04-05 17:59:38 +0900

    allow enhanced pin on startup(wip)

--- example/set-allow-enhanced-pins-for-startup.go
+++ example/set-allow-enhanced-pins-for-startup.go
@@ -1,8 +1,11 @@
 package main
 
 import (
-	go_gpedit "github.com/zeronsoftn/go-gpedit"
 	"log"
+
+	go_gpedit "github.com/zeronsoftn/go-gpedit"
+	"golang.org/x/sys/windows"
+	"golang.org/x/sys/windows/registry"
 )
 
 func main() {
@@ -13,7 +16,34 @@ func main() {
 
 	defer gpedit.Release()
 
-	if err := gpedit.OpenLocalMachineGPO(go_gpedit.GPO_OPEN_LOAD_REGISTRY); err != nil {
+	if err = gpedit.OpenLocalMachineGPO(go_gpedit.GPO_OPEN_LOAD_REGISTRY); err != nil {
+		log.Fatalln(err)
+	}
+
+	var keyMachine windows.Handle
+	if err = gpedit.GetRegistryKey(uint32(go_gpedit.GPO_SECTION_MACHINE), &keyMachine); err != nil {
+		log.Fatalln(err)
+	}
+
+	k, old, err := registry.CreateKey(registry.Key(keyMachine), "SOFTWARE\\Policies\\Microsoft\\FVE", registry.SET_VALUE)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if old {
+		log.Println("Overwriting existing value...")
+	}
+	defer k.Close()
+
+	err = k.SetDWordValue("UseEnhancedPin", 1)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if err = gpedit.Save(go_gpedit.TRUE,
+		go_gpedit.TRUE,
+		&go_gpedit.REGISTRY_EXTENSION_GUID,
+		&go_gpedit.CLSID_GPESnapIn,
+	); err != nil {
 		log.Fatalln(err)
 	}
 
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/zeronsoftn/go-gpedit
 
 go 1.18
 
-require golang.org/x/sys v0.6.0 // indirect
+require golang.org/x/sys v0.6.0
